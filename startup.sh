3afb36ce301d54c68851ec7675abb45a8bdaf0a1
Marcel Bajdel <m.bajdel@gsi.de>
2021-05-10 12:00:41 +0200

    Improvements to the CSS

--- startup.sh
+++ startup.sh
@@ -2,17 +2,15 @@
 
 set -e 
 
-
 #Defining colors 
 RED='\033[0;31m'
 NC='\033[0m'
 
-
-
 echo "${RED}Building the DCS${NC}"
 docker-compose -f docker-compose-aa.yml up -d 
 echo "${RED}Configuring alarm-server${NC}"
-docker exec -t alarm-server sh -c 'cd alarm-server && ./alarm-server.sh -config '$1' -import config/'$2''
+sleep 10s
+docker exec -t alarm-server sh -c 'cd alarm-server && ./alarm-server.sh -config '$1' -import config/'$2' -settings config/settings.ini'
 echo "${RED}Control System Studio is starting...${NC}"
 
 
